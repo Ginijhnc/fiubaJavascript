109574985880a2c0b7d80c607c0f9c161470bb71
Joel Hanc <11620838+Ginijhnc@users.noreply.github.com>
2019-11-08 17:53:47 -0300

    Add files via upload

---
+++ Dijkstra/dijkstra.js
@@ -0,0 +1,46 @@
+const vecinoCercano = (grid, x, y) => {
+
+  function crearArray(width, height) {
+    let arrayFinal = new Array(height);
+    let fila = new Array(width);
+    for(let iy = 0; iy < height; iy++) {
+        arrayFinal[iy] = fila.slice();
+    }
+    return arrayFinal;
+  }
+
+  function initBordes(arrayFinal, grid, width, height)  {
+    arrayFinal[0][0] = grid[0][0];
+    for (let ix = 1; ix < width; ix++) {
+      arrayFinal[ix][0] = arrayFinal[ix-1][0] + grid[ix][0];
+    }  
+    for (let iy = 1; iy < height; iy++) {
+      arrayFinal[0][iy] = arrayFinal[0][iy-1] + grid[0][iy];
+    }  
+  }
+
+  function rellenarArray(arrayFinal, grid, width, height) {
+    for (let ix = 1; ix < width; ix++) {
+      for (let iy = 1; iy < height; iy++) {
+        let pesoMinimo = Math.min(arrayFinal[ix-1][iy], 
+                                 arrayFinal[ix][iy-1]);
+        arrayFinal[ix][iy] = pesoMinimo + grid[ix][iy];
+      }
+    }
+  }
+
+  let width = x + 1, height = y + 1;
+  let arrayFinal = crearArray(width, height);
+  initBordes(arrayFinal, grid, width, height);
+  rellenarArray(arrayFinal, grid, width, height)
+  console.table(arrayFinal);
+
+  return arrayFinal[x, y];
+};
+
+/////////  INIT  /////////
+console.log(vecinoCercano( [
+  [1,2,3],
+  [4,8,2],
+  [1,5,3],
+], 2, 2) );
